970eea534f358f2dec4d61d4223f3f0d179dec36
Diego Colín <luiscolin764@gmail.com>
2024-11-14 15:24:42 -0600

    Prettier y ESlint configurados

--- react-native-blueye/App.js
+++ react-native-blueye/App.js
@@ -1,11 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableHighlight, TouchableOpacity, Pressable} from 'react-native';
+import { Button } from 'react-native-web';
+
+const icon = require('./assets/icon.png')
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>BluEye!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <TouchableOpacity 
+        underlayColor={"#09f"}
+        style={{
+          width: 200, 
+          height: 200,
+          backgroundColor:'red',
+          borderRadius:100,
+          justifyContent:'center',
+          alignItems:'center'
+        }}
+        onPress={() => alert('Hi')}
+      >
+        <Text style={{color:'white'}}>BluEye!</Text>
+      </TouchableOpacity>
+      <Pressable
+          onPress={() => {
+            setTimesPressed(current => current + 1);
+          }}
+          style={({pressed}) => [
+            {
+              backgroundColor: pressed ? 'rgb(210, 230, 255)' : 'white',
+            },
+            styles.wrapperCustom,
+          ]}>
+          {({pressed}) => (
+            <Text style={styles.text}>{pressed ? 'Pressed!' : 'Press Me'}</Text>
+          )}
+        </Pressable>
     </View>
   );
 }
@@ -13,7 +43,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
