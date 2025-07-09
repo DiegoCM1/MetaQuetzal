cc9be26630b3b698d4f280f44355e815ccb9698c
Diego Colín <luiscolin764@gmail.com>
2025-07-09 14:02:54 -0600

    Fixed navigation/Se eliminaron navigation trees generando issues de compatibilidad

--- react-native-blueye/app/_layout.jsx
+++ react-native-blueye/app/_layout.jsx
@@ -7,137 +7,135 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useColorScheme } from "nativewind";
 import { ThemeProvider } from "../context/ThemeContext";
 import { DaltonicModeProvider } from "../context/DaltonicModeContext";
-import {
-  DrawerActions,
-  useNavigation,
-} from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { DrawerActions, useNavigation } from "@react-navigation/native";
+import { Drawer } from "expo-router/drawer";
 import SettingsScreen from "./settings"; // ajusta la ruta si está en otro sub-folder
 import alarmScreen from "./alarmScreen"; // ajusta la ruta si está en otro sub-folder
 
+// function InnerApp() {
+//   const router = useRouter();
+//   const currentRoute = router?.pathname || "";
+//   const { colorScheme } = useColorScheme();
+//   const navigation = useNavigation();
+//   const MAIN_TABS = ["", "chat-ai", "alerts"];
 
-const Drawer = createDrawerNavigator(); // Create a Drawer Navigator
+//   const isMainTab = MAIN_TABS.some((path) => currentRoute.startsWith(path));
 
-function InnerApp() {
-  const router = useRouter();
-  const currentRoute = router?.pathname || "";
-  const { colorScheme } = useColorScheme();
-  const navigation = useNavigation();
-  const MAIN_TABS = ["", "chat-ai", "alerts"];
+//   const handleTopIconPress = () => {
+//     console.log("PATH", router.pathname, "SEG", router.segments);
 
-  const isMainTab = MAIN_TABS.some((path) => currentRoute.startsWith(path));
+//     // Si no estamos en un tab principal, volvemos al Home,  Si estamos en un tab principal, abrimos el Drawer
+//     if (isMainTab) {
+//       navigation.dispatch(DrawerActions.openDrawer());
+//       console.log("Opening drawer");
+//     } else if (navigation.canGoBack()) {
+//       navigation.goBack();
+//       console.log("Going back");
+//     } else {
+//       router.replace("/"); // fallback al Home
+//     }
+//   };
 
-  const handleTopIconPress = () => {
-    // Si no estamos en un tab principal, volvemos al Home,  Si estamos en un tab principal, abrimos el Drawer
-    if (isMainTab) {
-      navigation.dispatch(DrawerActions.openDrawer());
-      console.log("Opening drawer");
-    } else if (navigation.canGoBack()) {
-      navigation.goBack();
-      console.log("Going back");
-    } else {
-      router.replace("/"); // fallback al Home
-    }
-  };
+//   console.log("RENDER", router.pathname);
 
-  return (
-    <View className="flex-1 bg-phase2bg dark:bg-phase2bgDark">
-      {/* Top Bar */}
-      <View className="fixed top-0 left-0 right-0 flex-row items-center p-4 bg-phase2TopBar dark:bg-phase2TopBarDark z-10">
-        <MaterialCommunityIcons
-          name={isMainTab ? "menu" : "arrow-left"}
-          size={28}
-          onPress={handleTopIconPress}
-          color={
-            isMainTab && currentRoute === "/"
-              ? colorScheme === "dark"
-                ? "rgb(230, 230, 250)"
-                : "rgb(30, 30, 60)"
-              : "white"
-          }
-        />
-      </View>
+//   return (
+//     <View className="flex-1 bg-phase2bg dark:bg-phase2bgDark">
+//       {/* Top Bar */}
+//       <View className="fixed top-0 left-0 right-0 flex-row items-center p-4 bg-phase2TopBar dark:bg-phase2TopBarDark z-10">
+//         <MaterialCommunityIcons
+//           name={isMainTab ? "menu" : "arrow-left"}
+//           size={28}
+//           onPress={handleTopIconPress}
+//           color={
+//             isMainTab && currentRoute === "/"
+//               ? colorScheme === "dark"
+//                 ? "rgb(230, 230, 250)"
+//                 : "rgb(30, 30, 60)"
+//               : "white"
+//           }
+//         />
+//       </View>
 
-      {/* Slot */}
-      <Slot />
+//       {/* Slot */}
+//       <Slot />
 
-      {/* Bottom Tabs */}
-      <View className="w-full bg-phase2Cards dark:bg-phase2CardsDark border-t border-phase2Borders dark:border-phase2BordersDark">
-        <View className="flex-row">
-          {/* Botón Inicio/Mapa */}
-          <Link
-            href="/"
-            className={`flex flex-1 flex-col py-2 items-center text-center ${
-              currentRoute === "/"
-                ? "bg-phase2Cards dark:bg-phase2CardsDark text-phase2Titles dark:text-phase2TitlesDark"
-                : "bg-phase2Buttons dark:bg-phase2ButtonsDark text-white hover:bg-phase2Borders dark:hover:bg-phase2BordersDark"
-            }`}
-          >
-            <MaterialCommunityIcons
-              name="map"
-              size={28}
-              color={
-                currentRoute === "/"
-                  ? colorScheme === "dark"
-                    ? "rgb(230, 230, 250)" // phase2TitlesDark
-                    : "rgb(30, 30, 60)" // phase2Titles
-                  : "white"
-              }
-            />
+//       {/* Bottom Tabs */}
+//       <View className="w-full bg-phase2Cards dark:bg-phase2CardsDark border-t border-phase2Borders dark:border-phase2BordersDark">
+//         <View className="flex-row">
+//           {/* Botón Inicio/Mapa */}
+//           <Link
+//             href="/"
+//             className={`flex flex-1 flex-col py-2 items-center text-center ${
+//               currentRoute === "/"
+//                 ? "bg-phase2Cards dark:bg-phase2CardsDark text-phase2Titles dark:text-phase2TitlesDark"
+//                 : "bg-phase2Buttons dark:bg-phase2ButtonsDark text-white hover:bg-phase2Borders dark:hover:bg-phase2BordersDark"
+//             }`}
+//           >
+//             <MaterialCommunityIcons
+//               name="map"
+//               size={28}
+//               color={
+//                 currentRoute === "/"
+//                   ? colorScheme === "dark"
+//                     ? "rgb(230, 230, 250)" // phase2TitlesDark
+//                     : "rgb(30, 30, 60)" // phase2Titles
+//                   : "white"
+//               }
+//             />
 
-            <Text className="text-sm">Mapa</Text>
-          </Link>
+//             <Text className="text-sm">Mapa</Text>
+//           </Link>
 
-          {/* Botón Chat-AI */}
-          <Link
-            href="/chat-ai"
-            className={`flex flex-1 flex-col py-2 items-center text-center ${
-              currentRoute === "/chat-ai"
-                ? "bg-phase2Cards dark:bg-phase2CardsDark text-phase2Titles dark:text-phase2TitlesDark"
-                : "bg-phase2Buttons dark:bg-phase2ButtonsDark text-white hover:bg-phase2Borders dark:hover:bg-phase2BordersDark"
-            }`}
-          >
-            <MaterialCommunityIcons
-              name="robot-happy"
-              size={28}
-              color={
-                currentRoute.startsWith("/chat-ai")
-                  ? colorScheme === "dark"
-                    ? "rgb(230, 230, 250)" // phase2TitlesDark
-                    : "rgb(30, 30, 60)" // phase2Titles
-                  : "white"
-              }
-            />
-            <Text className="text-sm">Chat-AI</Text>
-          </Link>
+//           {/* Botón Chat-AI */}
+//           <Link
+//             href="/chat-ai"
+//             className={`flex flex-1 flex-col py-2 items-center text-center ${
+//               currentRoute === "/chat-ai"
+//                 ? "bg-phase2Cards dark:bg-phase2CardsDark text-phase2Titles dark:text-phase2TitlesDark"
+//                 : "bg-phase2Buttons dark:bg-phase2ButtonsDark text-white hover:bg-phase2Borders dark:hover:bg-phase2BordersDark"
+//             }`}
+//           >
+//             <MaterialCommunityIcons
+//               name="robot-happy"
+//               size={28}
+//               color={
+//                 currentRoute.startsWith("/chat-ai")
+//                   ? colorScheme === "dark"
+//                     ? "rgb(230, 230, 250)" // phase2TitlesDark
+//                     : "rgb(30, 30, 60)" // phase2Titles
+//                   : "white"
+//               }
+//             />
+//             <Text className="text-sm">Chat-AI</Text>
+//           </Link>
 
-          {/* Botón Configuración */}
-          <Link
-            href="/alerts"
-            className={`flex flex-1 flex-col py-2 items-center text-center justify-center ${
-              currentRoute === "/alerts"
-                ? "bg-phase2Cards dark:bg-phase2CardsDark text-phase2Titles dark:text-phase2TitlesDark"
-                : "bg-phase2Buttons dark:bg-phase2ButtonsDark text-white hover:bg-phase2Borders dark:hover:bg-phase2BordersDark"
-            }`}
-          >
-            <MaterialCommunityIcons
-              name="bell-alert-outline"
-              size={28}
-              color={
-                currentRoute.startsWith("/alerts")
-                  ? colorScheme === "dark"
-                    ? "rgb(230, 230, 250)" // phase2TitlesDark
-                    : "rgb(30, 30, 60)" // phase2Titles
-                  : "white"
-              }
-            />
-            <Text className="text-sm">Alertas</Text>
-          </Link>
-        </View>
-      </View>
-    </View>
-  );
-}
+//           {/* Botón Configuración */}
+//           <Link
+//             href="/alerts"
+//             className={`flex flex-1 flex-col py-2 items-center text-center justify-center ${
+//               currentRoute === "/alerts"
+//                 ? "bg-phase2Cards dark:bg-phase2CardsDark text-phase2Titles dark:text-phase2TitlesDark"
+//                 : "bg-phase2Buttons dark:bg-phase2ButtonsDark text-white hover:bg-phase2Borders dark:hover:bg-phase2BordersDark"
+//             }`}
+//           >
+//             <MaterialCommunityIcons
+//               name="bell-alert-outline"
+//               size={28}
+//               color={
+//                 currentRoute.startsWith("/alerts")
+//                   ? colorScheme === "dark"
+//                     ? "rgb(230, 230, 250)" // phase2TitlesDark
+//                     : "rgb(30, 30, 60)" // phase2Titles
+//                   : "white"
+//               }
+//             />
+//             <Text className="text-sm">Alertas</Text>
+//           </Link>
+//         </View>
+//       </View>
+//     </View>
+//   );
+// }
 
 /* ────────── TopBar global ────────── */
 function TopBar() {
@@ -180,16 +178,132 @@ function TopBar() {
 
 /* ---------- Layout raíz ---------- */
 export default function Layout() {
+  const router = useRouter();
+  const currentRoute = router?.pathname || "";
+  const { colorScheme } = useColorScheme();
+  const navigation = useNavigation();
+  const MAIN_TABS = ["", "chat-ai", "alerts"];
+
+  const isMainTab = MAIN_TABS.some((path) => currentRoute.startsWith(path));
+
+  const handleTopIconPress = () => {
+    console.log("PATH", router.pathname, "SEG", router.segments);
+
+    // Si no estamos en un tab principal, volvemos al Home,  Si estamos en un tab principal, abrimos el Drawer
+    if (isMainTab) {
+      navigation.dispatch(DrawerActions.openDrawer());
+      console.log("Opening drawer");
+    } else if (navigation.canGoBack()) {
+      navigation.goBack();
+      console.log("Going back");
+    } else {
+      router.replace("/"); // fallback al Home
+    }
+  };
+
   return (
     <DaltonicModeProvider>
       <ThemeProvider>
         <SafeAreaProvider>
           <TamaguiProvider config={config} defaultTheme="light">
-              <Drawer.Navigator screenOptions={{ headerShown: false }}>
-                <Drawer.Screen name="index" component={InnerApp} />
-                <Drawer.Screen name="settings" component={SettingsScreen} />
-                <Drawer.Screen name="alarmScreen" component={alarmScreen} />
-              </Drawer.Navigator>
+            <Drawer screenOptions={{ headerShown: false }}>
+              <Drawer.Screen name="index" />
+              <Drawer.Screen name="settings" />
+              <Drawer.Screen name="alarmScreen" />
+            </Drawer>
+            <View className="flex-1 bg-phase2bg dark:bg-phase2bgDark">
+              {/* Top Bar */}
+              <View className="fixed top-0 left-0 right-0 flex-row items-center p-4 bg-phase2TopBar dark:bg-phase2TopBarDark z-10">
+                <MaterialCommunityIcons
+                  name={isMainTab ? "menu" : "arrow-left"}
+                  size={28}
+                  onPress={handleTopIconPress}
+                  color={
+                    isMainTab && currentRoute === "/"
+                      ? colorScheme === "dark"
+                        ? "rgb(230, 230, 250)"
+                        : "rgb(30, 30, 60)"
+                      : "white"
+                  }
+                />
+              </View>
+
+
+              {/* Bottom Tabs */}
+              <View className="w-full bg-phase2Cards dark:bg-phase2CardsDark border-t border-phase2Borders dark:border-phase2BordersDark">
+                <View className="flex-row">
+                  {/* Botón Inicio/Mapa */}
+                  <Link
+                    href="/"
+                    className={`flex flex-1 flex-col py-2 items-center text-center ${
+                      currentRoute === "/"
+                        ? "bg-phase2Cards dark:bg-phase2CardsDark text-phase2Titles dark:text-phase2TitlesDark"
+                        : "bg-phase2Buttons dark:bg-phase2ButtonsDark text-white hover:bg-phase2Borders dark:hover:bg-phase2BordersDark"
+                    }`}
+                  >
+                    <MaterialCommunityIcons
+                      name="map"
+                      size={28}
+                      color={
+                        currentRoute === "/"
+                          ? colorScheme === "dark"
+                            ? "rgb(230, 230, 250)" // phase2TitlesDark
+                            : "rgb(30, 30, 60)" // phase2Titles
+                          : "white"
+                      }
+                    />
+
+                    <Text className="text-sm">Mapa</Text>
+                  </Link>
+
+                  {/* Botón Chat-AI */}
+                  <Link
+                    href="/chat-ai"
+                    className={`flex flex-1 flex-col py-2 items-center text-center ${
+                      currentRoute === "/chat-ai"
+                        ? "bg-phase2Cards dark:bg-phase2CardsDark text-phase2Titles dark:text-phase2TitlesDark"
+                        : "bg-phase2Buttons dark:bg-phase2ButtonsDark text-white hover:bg-phase2Borders dark:hover:bg-phase2BordersDark"
+                    }`}
+                  >
+                    <MaterialCommunityIcons
+                      name="robot-happy"
+                      size={28}
+                      color={
+                        currentRoute.startsWith("/chat-ai")
+                          ? colorScheme === "dark"
+                            ? "rgb(230, 230, 250)" // phase2TitlesDark
+                            : "rgb(30, 30, 60)" // phase2Titles
+                          : "white"
+                      }
+                    />
+                    <Text className="text-sm">Chat-AI</Text>
+                  </Link>
+
+                  {/* Botón Configuración */}
+                  <Link
+                    href="/alerts"
+                    className={`flex flex-1 flex-col py-2 items-center text-center justify-center ${
+                      currentRoute === "/alerts"
+                        ? "bg-phase2Cards dark:bg-phase2CardsDark text-phase2Titles dark:text-phase2TitlesDark"
+                        : "bg-phase2Buttons dark:bg-phase2ButtonsDark text-white hover:bg-phase2Borders dark:hover:bg-phase2BordersDark"
+                    }`}
+                  >
+                    <MaterialCommunityIcons
+                      name="bell-alert-outline"
+                      size={28}
+                      color={
+                        currentRoute.startsWith("/alerts")
+                          ? colorScheme === "dark"
+                            ? "rgb(230, 230, 250)" // phase2TitlesDark
+                            : "rgb(30, 30, 60)" // phase2Titles
+                          : "white"
+                      }
+                    />
+                    <Text className="text-sm">Alertas</Text>
+                  </Link>
+                </View>
+              </View>
+            </View>
           </TamaguiProvider>
         </SafeAreaProvider>
       </ThemeProvider>
