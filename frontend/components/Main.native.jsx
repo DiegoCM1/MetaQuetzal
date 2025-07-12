2addecf2b4ebb9ff784f11a130420e3bc82452d3
Diego Colín <luiscolin764@gmail.com>
2025-07-11 18:21:33 -0600

    Feature/Se hizo separacion de mapa web y mapa native en Main.tipo.jsx debido a que leaflet no funciona en native

---
+++ frontend/components/Main.native.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { View, Text } from "react-native";
+export default function Main() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Map screen is web-only for now 👍</Text>
+    </View>
+  );
+}
