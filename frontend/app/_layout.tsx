1f4b8827a7f5364f8ce5dcdb691aed26026a5ec6
Luis Diego Colin <luiscolin764@gmail.com>
2026-05-06 17:28:25 -0600

    chore: added dynamic dsn for Sentry in .env and added example in .env.example

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -30,7 +30,7 @@ import { usePathname } from "expo-router";
 import * as Sentry from '@sentry/react-native';
 
 Sentry.init({
-  dsn: 'https://89737012832b41feec4b2bbf7f53d7e1@o4511345134403584.ingest.us.sentry.io/4511345138728960',
+  dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
 
   // Adds more context data to events (IP address, cookies, user, etc.)
   // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
